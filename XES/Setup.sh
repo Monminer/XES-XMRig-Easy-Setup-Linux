9c40313f18dc246c8ae32cd087834b242e8302d2
Monminer <118969380+Monminer@users.noreply.github.com>
2022-11-24 16:00:15 +0530

    Add files via upload

---
+++ XES/Setup.sh
@@ -0,0 +1,22 @@
+echo Welcome to XEM!
+echo Installing required things....
+sudo apt install jq cpufrequtils -y
+echo Cleaning up...
+mkdir tmp
+mv Setup.sh tmp
+mv start.sh tmp
+rm *
+rm -r xmrig*
+cd tmp
+mv Setup.sh ..
+mv start.sh ..
+cd ..
+echo Installing XMRig....
+cd tmp
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-focal-x64.tar.gz
+cd ..
+tar -xvf tmp/xmrig*
+cd tmp
+rm *
+cd ..
+cd XEM_req && sh config.sh
