e945f013f863b621ce62f35836978f2a7c6e79f5
Monminer <118969380+Monminer@users.noreply.github.com>
2022-11-24 15:47:58 +0530

    Add files via upload

---
+++ XES/XEM_req/XEM.sh
@@ -0,0 +1,21 @@
+echo Welcome to XEM!
+echo Setting CPU speed to quick!
+sudo cpufreq-set -c 0 -g performance
+sudo cpufreq-set -c 1 -g performance
+echo Loading Config.....
+wallet=$(cat config.json | jq -r '.config.wallet')
+pool=$(cat config.json | jq -r '.config.pool')
+threads=$(cat config.json | jq -r '.config.threads')
+rigid=$(cat config.json | jq -r '.config.rigid')
+echo
+echo Starting "in" 
+echo 3
+sleep 1
+echo 2
+sleep 1
+echo 1
+sleep 1
+echo Start!
+cd ..
+cd xmrig*
+sudo ./xmrig -o $pool -u $wallet -t $threads -p $rigid
